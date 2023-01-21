b780992f7925acd7838b0ff845638d8710153a55
alexac54767 <alexarosecarlson@icloud.com>
2023-01-20 22:53:06 -0800

    fixing main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-# @app.route('/')  # connects default URL to index() function
-# def index():
-    # return render_template("index.html")
+@app.route('/')  # connects default URL to index() function
+def index():
+     return render_template("index.html")
 
 # @app.route('/stub/')  # connects /stub/ URL to stub() function
 # def stub():
