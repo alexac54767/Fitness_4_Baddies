154165c6a37e5446171d7357f1eabc28dc56e954
alexac54767 <alexarosecarlson@icloud.com>
2023-01-20 22:18:31 -0800

    adding morts api file

--- api/backupuser.py
+++ api/backupuser.py
@@ -25,10 +25,12 @@ class UserAPI:
             uid = body.get('uid')
             if uid is None or len(uid) < 2:
                 return {'message': f'User ID is missing, or is less than 2 characters'}, 210
-            # look for password and dob
+            # look for password 
             password = body.get('password')
-            dob = body.get('dob')
-
+            workouts = body.get('workouts')
+            if workouts is None or len(workouts) < 2:
+                 return {'message': f'Workouts is missing, or is less than 2 characters'}, 210
+                 
             ''' #1: Key code block, setup USER OBJECT '''
             uo = User(name=name, 
                       uid=uid)
@@ -37,12 +39,6 @@ class UserAPI:
             # set password if provided
             if password is not None:
                 uo.set_password(password)
-            # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%m-%d-%Y').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
             
             ''' #2: Key Code block to add user to database '''
             # create user in database
